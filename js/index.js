2209e7716fbb92b71e0a275fb28c679268c3b99c
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-03-07 00:29:49 +0300

    ft:Input validation - Verify date inputs of the user functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,67 @@
-let viewOnLoad = ()=>
+const formInfo = document.getElementById("inform");
+
+const day = document.getElementById("dayInput");
+const month = document.getElementById("monthInput");
+const year = document.getElementById("yearInput");
+
+
+
+
+
+const male = document.getElementById("male");
+const female = document.getElementById("female");
+const btn = document.getElementById("subtn");
+const par = document.getElementById("akaname");
+
+const genderMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const genderFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
+
+
+
+
+
+
+
+let getAkanName = ()=>
 {
 
+    // Getting the date format and joining the inputs
+
+    let inputDate = [year.value, month.value,day.value].join()
+
+    let dateNew = new Date(inputDate)
+    const dayOfWeek = dateNew.getDay();
+
+
+    // Verifying  Inputs
+    if(day.value == " " || day.value >31)
+    {
+        alert("Enter Valid day between 1 -31")
+    }
+    else if(month.value == " " || month.value >12)
+    {
+        alert("Enter a Valid Month between 1 - 12")
+    }
+    else if(year.value.length == " "|| year.value.length <4)
+    {
+        alert("Enter a valid Year")
+    }
+
+
+
+
+
+
 }
+
+getAkanName();
+
+btn.addEventListener("click", getAkanName)
+
+
+
+
+
+
